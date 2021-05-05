00bc5f54c959123478b623a60ab0506109441ce7
wzc0x0 <wzc0x0@gmail.com>
2021-05-05 22:37:46 +0800

    切换连接地址

--- src/utils/socketClient.ts
+++ src/utils/socketClient.ts
@@ -1,8 +1,10 @@
 import { io } from 'socket.io-client';
 //192.168.199.183:3000
-//locahost:3000
-const socket = io('192.168.199.183:3000', {
+//localhost:3000
+//hk.19921024.xyz:3000
+const socket = io('https://hk.19921024.xyz', {
   reconnectionDelayMax: 10,
+  secure: true,
 });
 
 export default socket;
